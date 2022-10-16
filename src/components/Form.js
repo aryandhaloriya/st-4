0471ec42e3bc90056afad6c9fae8575769bc1215
aryandhaloriya <105759223+aryandhaloriya@users.noreply.github.com>
2022-10-16 21:01:52 +0530

    Add files via upload

---
+++ src/components/Form.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import Cricketers from './Cricketers';
+import './Form.css';
+import { Link, useNavigate } from 'react-router-dom';
+import cricketer from './cricketer.jpg';
+import cup from './cup.png'
+
+
+export default function Form() {
+  const [position, setposition] = useState('');
+  const [player, setplayer] = useState('');
+  const [rating, setrating] = useState('');
+  const[role,setrole]=useState('');
+
+  let history = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let a = position, b = player, c = rating,d =role;
+    Cricketers.push({ a, b, c, d });
+    // console.log(Cricketers);
+    history('/added');
+
+
+  }
+
+  return (
+    <div>
+      <h1>CRICKETER'S WALL OF FAME</h1>
+      <form className='container'>
+        <label>Position</label><br></br>
+        <input type="number"   placeholder='Enter Position' onChange={(e) => setposition(e.target.value)} required></input><br></br>
+        <label>Player</label><br></br>
+        <input type="text"   placeholder='Enter Name' onChange={(e) => setplayer(e.target.value)} required></input><br></br>
+        <label >Rating</label><br></br>
+        <input type="number" placeholder='Enter Rating' onChange={(e) => setrating(e.target.value)} required></input><br></br>
+        <label >Role</label><br></br>
+        <input type="text" placeholder='Enter Role' onChange={(e) => setrole(e.target.value)} required></input><br></br>
+
+        <Link to='/added'>
+          <input className='btn' type="submit" onClick={handleSubmit}></input>
+        </Link>
+
+      </form>
+
+      <img className='imagecup' src={cup}></img>
+      <img className='image' src={cricketer}></img>
+
+    </div>
+  )
+}
